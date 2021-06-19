80a99ca74be31c875da1bc15b55706b7d3fa632d
Pooja123113 <85823029+Pooja123113@users.noreply.github.com>
2021-06-19 03:31:47 -0700

    program on function.

---
+++ function 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void sub()
+{
+   int x=30,y=15;
+   cout<<"\nsubstraction:" << x-y;
+}
+int main()
+{
+int a=20,b=30;
+cout<<"addition:"<<a+b;
+sub();
+sub();
+return 0;
+
+}
