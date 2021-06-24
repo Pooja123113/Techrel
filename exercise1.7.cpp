cabcbb5817ec68999827f7cc84553b40b5306727
Pooja123113 <85823029+Pooja123113@users.noreply.github.com>
2021-06-23 22:15:31 -0700

    Program on arithmetic operation with different data types

---
+++ exercise1.7.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+    {
+    int a=5,b=7;
+    float c=3.7,d=8.0;
+
+    cout<<"Display arithmetic operation with mixed data type:"<<endl;
+
+    cout<<a<<"+"<<b<<"="<<a+b<<endl;
+    cout<<c<<"+"<<d<<"="<<c+d<<endl;
+    cout<<a<<"+"<<d<<"="<<a+d<<endl;
+    cout<<a<<"-"<<b<<"="<<a-b<<endl;
+    cout<<c<<"-"<<d<<"="<<c-d<<endl;
+    cout<<a<<"-"<<d<<"="<<a-d<<endl;
+    cout<<a<<"*"<<b<<"="<<a*b<<endl;
+    cout<<c<<"*"<<d<<"="<<c*d<<endl;
+    cout<<a<<"*"<<d<<"="<<a*d<<endl;
+    cout<<a<<"/"<<b<<"="<<a/b<<endl;
+    cout<<c<<"/"<<d<<"="<<c/d<<endl;
+    cout<<a<<"/"<<d<<"="<<a/d<<endl;
+
+    return 0;
+    }
